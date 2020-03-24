8f955c960bc0f447acc00f9b55292be304228a62
vagrant <flora.uwadiegwu@gmail.com>
2020-03-24 20:26:49 +0000

    jQuery function for the counter in Tweeter App

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  //as user types we want to change counter
+
+  //Step 1: how many char they've typed so far
+  //Step 2: how many char they have left based on step 1
+  //Step 3: update the counter based on new number in step 2
+  //$(event.target) = this
+
+  $("#tweet-text").on('keyup', (event) => {
+    const stringTyped = $(event.target).val().length;
+    const remain = 140 - stringTyped;
+    $(".counter").text(remain);
+
+    if (remain < 0) {
+      $(".counter").addClass("error-counter");
+    } else {
+      $(".counter").removeClass("error-counter");
+    }
+  })
+});
